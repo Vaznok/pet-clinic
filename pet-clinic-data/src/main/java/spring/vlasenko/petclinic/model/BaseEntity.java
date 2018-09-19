2dbcd5b4172a975ba78efc8e2450ac410b5406ad
Vitaliy Vlasenko <vlasenko.vitaliy.l@gmail.com>
2018-09-19 13:13:55 +0300

    Junit 5

--- pet-clinic-data/src/main/java/spring/vlasenko/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/spring/vlasenko/petclinic/model/BaseEntity.java
@@ -1,6 +1,9 @@
 package spring.vlasenko.petclinic.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- pet-clinic-data/src/main/java/spring/vlasenko/petclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/spring/vlasenko/petclinic/service/map/AbstractMapService.java
@@ -4,8 +4,8 @@ import spring.vlasenko.petclinic.model.BaseEntity;
 
 import java.util.*;
 
-public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
-    protected Map<Long, T> map = new HashMap<>();
+abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
